fdc26a93c496e154276118172dc22ec16e24f7f4
Roshini George <theroshinigeorge@gmail.com>
2017-03-04 20:55:52 +0000

    Initial commit for our project

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!-- This is the basic webpage-->
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<!-- set default language -->
+<html lang="en">
+<!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
+<head>  
+</head> 
+<!-- body: where we will put the stuff we can see! -->
+<body>
+<h1>Hello World!</h1>
+<p>Boogity boogity boogity blorp blorp blorp bleep bleep bleep <br> Baa baa black sheep</p>
+<img src="http://31.media.tumblr.com/ffebeac051f9e17de12d1ce47d426271/tumblr_mxq0ewfa3i1s46koto1_500.gif" alt="Nostalgia critic sitting at a desk and saying: people may die but stupidity is forever"
+</body>
