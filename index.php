4525ba00f8cef8e63ee3c2be1122274bb5bb3d5f
Roshini George <theroshinigeorge@gmail.com>
2017-03-04 21:15:56 +0000

    added bootstrap and jquery

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
-
 <!-- This is the basic webpage-->
 <!-- We call it a hypertext markup language document -->
 <!DOCTYPE html>
 <!-- set default language -->
 <html lang="en">
 <!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
-<head>  
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+  <!-- These scripts give us access to the bootstrap font end framework, which makes styling pretty easy!  -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head> 
 <!-- body: where we will put the stuff we can see! -->
 <body>
-<h1>Hello World!</h1>
+  <!-- suddenly, we can style our stuff with bootstrap classes!  -->
+    <h1>Hello World!</h1> 
+      
+</body>
+
 <p>Boogity boogity boogity blorp blorp blorp bleep bleep bleep <br> Baa baa black sheep</p>
 <img src="http://31.media.tumblr.com/ffebeac051f9e17de12d1ce47d426271/tumblr_mxq0ewfa3i1s46koto1_500.gif" alt="Nostalgia critic sitting at a desk and saying: people may die but stupidity is forever"
 </body>
